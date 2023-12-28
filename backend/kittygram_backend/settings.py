cb4ca1dc2b02d8f50874bb696843e0b28cfb4867
lanarkk <slavabogurandoma@mail.ru>
2023-12-28 10:13:57 +0300

    accorfing to flake8

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ if os.getenv('DEBUG', '').lower == 'true':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ['localhost^127.0.0.1']).split('^')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost^127.0.0.1').split('^')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
